353c64028a48c672972f62bd0e53ffba11abc6e3
James Nelson <jamestn529@gmail.com>
2021-10-14 20:20:51 -0600

    Add flags to build script and remove unused code

--- build.sh
+++ build.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
 CC=clang
-CFLAGS=""
+CFLAGS="-Wall -O2"
 
 gperf -S 1 src/prim.gperf > src/prim.h
 $CC $CFLAGS src/lith.c src/main.c -ledit -o out/lith
-
-# case "$1" in
-#     "")
-#         ;;
-#     run)
-#         out/lith
-#         ;;
-#     *)
-#         echo "unknown command $1"
-#         ;;
-# esac
