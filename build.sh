b31014003ccf34198ffb152d978fc7d67fcf4f27
James Nelson <jamestn529@gmail.com>
2021-10-14 21:07:18 -0600

    Add "run" and "clean" commands to build script

--- build.sh
+++ build.sh
@@ -6,3 +6,15 @@ CFLAGS="-Wall -O2"
 mkdir -p out
 gperf -S 1 src/prim.gperf > src/prim.h
 $CC $CFLAGS src/lith.c src/main.c -ledit -o out/lith
+
+for command in "$@"
+do
+    case $command in
+    run)
+        out/lith
+        ;;
+    clean)
+        rm -rf out
+        ;;
+    esac
+done
