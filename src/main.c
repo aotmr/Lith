b626158dfffea6731dd260f32192278d477e17e5
James Nelson <jamestn529@gmail.com>
2021-10-14 21:14:20 -0600

    Introduce argp for argument parsing

--- src/main.c
+++ src/main.c
@@ -1,11 +1,15 @@
 #include "lith.h"
 
+#include <argp.h>
 #include <assert.h>
 #include <histedit.h>
 #include <stdio.h>
 
 int main(int argc, char * * argv)
 {
+    error_t error = argp_parse(NULL, argc, argv, 0, NULL, NULL);
+    (void)error;
+    
     lith_State *st = lith_create(&(lith_CreateOptions){ 0x10000, 32, 32 });
     assert(st);
 
